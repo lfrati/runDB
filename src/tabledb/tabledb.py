7d6a8f919bc9a6c7cb0b2e3c5777627ca9d3042f
lfrati <fratilapo@gmail.com>
2023-11-07 10:55:18 -0500

    Only keep connection while reading/writing
    Before the connections was opened as objects were instantiated.
    This made it so concurrent writes would timeout for long running jobs.
    Now the connection is opened and closed on every insertion, deletion, or query.
    The purpose is not to have high performance operations, but to allow for
    many concurrent workers to experiment results in a common database.

--- src/tabledb/tabledb.py
+++ src/tabledb/tabledb.py
@@ -1,20 +1,18 @@
 import sqlite3
 
-# import pandas as pd
-
 
 class tableDB:
     def __init__(self, file):
-        self.db = file
-        self.con = sqlite3.connect(file)
-        self.cur = self.con.cursor()
+        self.db_file = file
         self.table = "data"
 
-        try:
-            self.cur.execute(f"SELECT * FROM {self.table}")
-            self.columns = [cols[0] for cols in self.cur.description]
-        except sqlite3.OperationalError:
-            self.columns = []
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            try:
+                cur.execute(f"SELECT * FROM {self.table}")
+                _ = [cols[0] for cols in cur.description]
+            except sqlite3.OperationalError:
+                self.columns = []
 
     def create(self, *columns):
         """
@@ -26,8 +24,10 @@ class tableDB:
         self.cmd = (
             f"CREATE TABLE IF NOT EXISTS {self.table}({', '.join(columns)}, {pr_key})"
         )
-        self.cur.execute(self.cmd)
-        self.con.commit()
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(self.cmd)
+            con.commit()
 
     def insertall(self, entries):
         for entry in entries:
@@ -36,8 +36,11 @@ class tableDB:
     def insertone(self, entry):
         placeholders = ",".join(["?" for _ in self.columns])
         data = [entry[col] for col in self.columns]
-        self.cur.execute(f"INSERT INTO {self.table} VALUES({placeholders})", data)
-        self.con.commit()
+        cmd = f"INSERT INTO {self.table} VALUES({placeholders})"
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            result = cur.execute(cmd, data)
+            return result
 
     def deleteall(self, col, values):
         for val in values:
@@ -45,53 +48,18 @@ class tableDB:
 
     def deleteone(self, col, val):
         cmd = f"DELETE FROM {self.table} WHERE {col}='{val}';"
-        self.cur.execute(cmd)
-        self.con.commit()
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(cmd)
+            con.commit()
 
     def query(self, col, name):
         assert (
             len(self.columns) > 0
         ), "Error: table has not been initialized yet. Please use: create(table, *columns) first."
-        self.cur.execute(f"SELECT * FROM {self.table} WHERE {col} = ?", (name,))
-        results = self.cur.fetchall()
-        if len(results) > 0:
+
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(f"SELECT * FROM {self.table} WHERE {col} = ?", (name,))
+            results = cur.fetchall()
             return [{k: v for k, v in zip(self.columns, result)} for result in results]
-        return []
-
-    # def to_df(self):
-    #     self.cur.execute(f"SELECT * FROM {self.table}")
-    #     rows = self.cur.fetchall()
-    #     df = pd.DataFrame(columns=self.columns, data=rows)
-    #     return df
-
-
-#
-# db = tableDB("test.db")
-#
-# db.create("name", "lr", "acc")
-#
-# db.insertone({"name": "zio", "lr": 0.001, "acc": 0.98})
-#
-# try:
-#     db.insertall(
-#         [
-#             {"name": "zio", "lr": 0.001, "acc": 0.98},
-#             {"name": "zio", "lr": 0.001, "acc": 0.98},
-#         ]
-#     )
-# except sqlite3.IntegrityError:
-#     print("CAN'T INSERT MULTIPLE ZIOs")
-#
-# try:
-#     db.insertall(
-#         [
-#             {"name": "zio", "lr": 0.001, "acc": 0.98},
-#             {"name": "zio", "lr": 0.001, "acc": 0.98},
-#         ]
-#     )
-# except sqlite3.IntegrityError:
-#     print("CAN'T INSERT MULTIPLE ZIOs")
-#
-# print(db.query("name", "zio"))
-#
-# print(db.to_df())
--- tests/test_tabledb.py
+++ tests/test_tabledb.py
@@ -1,6 +1,10 @@
-import pytest
-import tempfile
+from multiprocessing import Process
+from random import random
 import sqlite3
+import tempfile
+from time import sleep
+
+import pytest
 
 from tabledb import tableDB
 
@@ -64,3 +68,40 @@ def test_delete(fulldb):
 
     results = fulldb.query("name", "zio")
     assert len(results) == 0
+
+
+def test_concurr_write(emptydb):
+    emptydb.create("name", "lr", "acc")  # NOTE: don't pass them as a list!
+
+    def work(db, entry):
+        t = random()
+        sleep(t)
+        db.insertone(entry)
+
+    import resource
+
+    nfiles, _ = resource.getrlimit(resource.RLIMIT_NOFILE)
+    # each worker uses 2 files (1 to subprocess, 1 to open db), let's be safe
+    N = nfiles // 3
+
+    procs = []
+    entries = [
+        {"name": f"worker_{i}", "lr": random(), "acc": random()} for i in range(N)
+    ]
+
+    for entry in entries:
+        proc = Process(target=work, args=(emptydb, entry))
+        procs.append(proc)
+        proc.start()
+
+    # complete the processes
+    for proc in procs:
+        proc.join()
+
+    for entry in entries:
+        results = emptydb.query("name", entry["name"])
+        assert len(results) == 1
+        retrieved = results[0]
+        assert all(
+            [entry[key] == retrieved[key] for key in entry.keys()]
+        ), f"ERROR: {entry} != {retrieved}"
